2a0a1470570e64f757dee827e04b1308d7b8bc66
Ahmed Nasri <23P0270@eng.asu.edu.eg>
2026-03-16 15:05:44 +0200

    Add changes variable to app.js

--- Lab9/app.js
+++ Lab9/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Course = require('./courses');
-
+const changes = 1;
 mongoose.connect('mongodb://localhost:27017/lab7');
 const app = express();
 app.use(bodyParser.json());
